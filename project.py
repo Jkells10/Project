a5d3496e7e7445085277c25b119c82f364a10ad1
Jkells10 <kelly.jordan@stthom.edu>
2024-12-02 13:42:33 -0600

    Add files via upload

---
+++ project.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+for _ in range(10):
+    print(f"Hello, {name}!")
+print("Sorry, there was an error.")
+name = input("Enter your name once again: ")
+print(f"Welcome {name}! What can I help you with today?")
